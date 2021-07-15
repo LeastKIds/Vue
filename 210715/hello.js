052ad0919568113076a7dfee6ea97fc6b69c4217
leastkids <leastkids@gmail.com>
2021-07-15 16:51:01 +0900

    210715 form function

--- 210715/hello.js
+++ 210715/hello.js
@@ -1,6 +1,31 @@
 let app = new Vue({
-    el : '#title',
+    el : '#app',
     data : {
-        message : 'Hello'
-    }
+        counter : 0,
+        message : 'Select a user',
+        users : [ {name : 'User'}, {name : "Admin"}],
+        content : '',
+        comment : '',
+        value : false,
+        values : [],
+
+        value_2 : 'hello',
+        values_2 : 'hello',
+
+        age : 20,
+
+        message_2 : '',
+
+    },
+    methods : {
+        addCounter() {
+            this.counter +=1;
+        },
+        subCounter() {
+            this.counter -=1;
+        }
+        ,sayHi(name) {
+            this.message = `Hi, ${name}`;
+        }
+    },
 });
